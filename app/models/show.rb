c1f4e7942bc16c3c414b31de413bc33f3173f08c
Donovan Jones <donovanljones94@gmail.com>
2019-11-07 14:33:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,9 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
   
+  def self.most_popular_show
+    high_num = Show.highest_rating
+    Show.where("rating = ?", high_num)
+  end
+  
 end
